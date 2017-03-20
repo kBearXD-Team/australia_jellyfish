6b4ea11af481712732d97bf74b524e34b61c6891
phisixersai <phisixersai@gmail.com>
2017-03-21 02:52:28 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const botBuilder = require('claudia-bot-builder');
+const excuse = require('huh');
+  
+module.exports = botBuilder(function(message) {
+    return 'Thanks for sending '+ message.text +' Your message is very important to us, but '+ excuse.get();
+});
